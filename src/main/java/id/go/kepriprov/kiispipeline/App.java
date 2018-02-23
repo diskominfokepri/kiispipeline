57239a64b73076ce5629ff7670ff0534a14c086d
KIIS <KIIS@DESKTOP-J792NL2>
2018-02-23 10:05:36 +0700

    create new scheduler

--- src/main/java/id/go/kepriprov/kiispipeline/App.java
+++ src/main/java/id/go/kepriprov/kiispipeline/App.java
@@ -1,9 +1,21 @@
 package id.go.kepriprov.kiispipeline;
 
-public class App {
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+public class App {
+		
 	public static void main(String[] args) {
-		System.out.println ("KIIS Pipeline");
+		Runnable runnable = new Runnable() {
+			public void run() {
+				// task to run goes here
+				System.out.println("Hello !!");
+			}
+		};
+		
+		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+		service.scheduleAtFixedRate(runnable, 0, 1, TimeUnit.SECONDS);
 	}
 
 }
